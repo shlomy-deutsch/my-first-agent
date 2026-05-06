2874106647458530ee0df5d49c58792ce1d448ee
Shlomi Deutsch <s0583285252@gmail.com>
2026-05-06 11:23:38 +0300

    test again ci cd

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       HI THERE i did change
+      now i have to test it again
     </div>
   );
 }
